460fe43adc000f6eb3b738a9d3774f1d70a932fd
Ben McLean <ben@mclean.one>
2016-08-13 17:55:13 +1000

    Fixed bug where iterator would iterate past end

--- SuperRoulette/Highscores.cpp
+++ SuperRoulette/Highscores.cpp
@@ -10,7 +10,7 @@ Highscores::Highscores(string file){
 vector<Highscore *> Highscores::get(int number){
 	vector<Highscore *> h;
 
-	for(list<Highscore>::iterator it = highscores.begin(), int count = 0; count < number; it++, count++){
+	for(list<Highscore>::iterator it = highscores.begin(), int count = 0; count < number && it != highscores.end(); it++, count++){
 		h.push_back(&(*it));
 	}
 
